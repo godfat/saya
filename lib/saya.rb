6745487fa116f1ae40c11e24cb986858f8dd6553
Lin Jen-Shin <godfat@godfat.org>
2013-07-11 16:43:31 +0800

    this is awful, middleware in map block would get inited each time

--- lib/saya.rb
+++ lib/saya.rb
@@ -3,18 +3,14 @@ require 'saya/api'
 require 'rack'
 
 Saya::Root   = File.expand_path("#{__dir__}/..")
-Saya::Server = Rack::Builder.new do
+Saya::Server = Rack::Builder.app do
   use Rack::ContentLength
   use Rack::ContentType
-
-  map '/api' do
-    run Saya::API.new
+  use Rack::Session::Pool
+  use Rack::Config do |env|
+    env['PATH_INFO'] = 'index.html' if env['PATH_INFO'] == '/'
   end
 
-  map '/' do
-    use Rack::Config do |env|
-      env['PATH_INFO'] = 'index.html' if env['PATH_INFO'] == '/'
-    end
-    run Rack::File.new("#{Saya::Root}/public")
-  end
+  map '/api' do run Saya::API.new                          end
+  map '/'    do run Rack::File.new("#{Saya::Root}/public") end
 end
